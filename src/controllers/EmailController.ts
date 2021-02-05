36f077a3d1917e80eb46cd2913689798879f0e89
Antonio Raimundo <antoniorrm@sysautomacao.com.br>
2021-02-05 15:30:58 -0300

    update response and send to all emails

--- src/controllers/EmailController.ts
+++ src/controllers/EmailController.ts
@@ -4,34 +4,40 @@ import { AWS_SES } from "../server";
 
 export default {
   async sendEmailAws(request: Request, response: Response) {
-    const { from, to, body } = request.body;
-    let params = {
-      Source: from,
-      Destination: {
-        ToAddresses: to,
-      },
-      ReplyToAddresses: [],
-      Message: {
-        Body: {
-          Html: {
+    const { from, to, subject, body } = request.body;
+    try {
+      let params = {
+        Source: from,
+        Destination: {
+          ToAddresses: to,
+        },
+        ReplyToAddresses: [],
+        Message: {
+          Body: {
+            Html: {
+              Charset: "UTF-8",
+              Data: body,
+            },
+          },
+          Subject: {
             Charset: "UTF-8",
-            Data: body,
+            Data: subject,
           },
         },
-        Subject: {
-          Charset: "UTF-8",
-          Data: `Hello, ${to}!`,
-        },
-      },
-    };
-    AWS_SES.sendEmail(params)
-      .promise()
-      .then(() => {
-        return response.status(200);
-      })
-      .catch((error) => {
-        return response.status(error.statusCode).json(error);
-      });
+      };
+      AWS_SES.sendEmail(params)
+        .promise()
+        .then((res) => {
+          return response.status(200).json({ message: "enviado com sucesso" });
+        })
+        .catch((error) => {
+          return response
+            .status(error.statusCode ? error.statusCode : 400)
+            .json(error);
+        });
+    } catch (error) {
+      return response.status(500).json({ message: "Erro interno" });
+    }
   },
 
   async sendEmailTemplateAws(request: Request, response: Response) {
